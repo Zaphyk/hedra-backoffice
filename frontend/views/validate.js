6687fd37fc3a874ba9a114fd9a1e0a0e6ce156e5
Zaphyk <zaphyk@live.com>
2017-10-26 10:22:05 -0300

    validate.js now supports testmode

--- frontend/views/validate.js
+++ frontend/views/validate.js
@@ -1,6 +1,8 @@
 
 
 function validate(){
+	if(testMode) return;
+	
 	jQuery.ajaxSetup({async:false});
     $.get("https://hedra-account-system.herokuapp.com/request?type=validate_token&token="+Cookies.get("token")+"&user="+Cookies.get("user"), function (data) {
     	if(JSON.parse(data) == false){
